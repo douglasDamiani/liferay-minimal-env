d86b767065b30ad60799490beb91619c6edb85c9
Douglas Damiani <douglas.damiani@liferay.com>
2024-04-29 15:21:04 -0300

    comment script code for fix CX static file problem

--- docker/mysql/fix-client-extension/99-clear-registry-in-table-configurations.sh
+++ docker/mysql/fix-client-extension/99-clear-registry-in-table-configurations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run MySQL with the --force option
-mysql -u "root" -p"$MYSQL_PASSWORD" -e "USE lportal; DELETE IGNORE FROM Configuration_;" --force || true
+# mysql -u "root" -p"$MYSQL_PASSWORD" -e "USE lportal; DELETE IGNORE FROM Configuration_;" --force || true
